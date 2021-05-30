075537dc60cb5a12ca8fb9ccb93678ec08cbf75c
Connor DiPietro <connor.dipietro@gmail.com>
2021-05-30 14:55:39 -0400

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
+
 const router = require("express").Router();
+const generatePassword = require('password-generator');
 const testDbItem = require("../models/test-model.js");
 const testDbItemData = require("../dev/test-data.js")
 
@@ -25,6 +27,20 @@ router.get("/test-data", (req, res) => {
   testDbItem.find({})
     .then((results) => res.send(results))
     .catch((err) => res.send(err))
+});
+
+router.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
 });
 
 module.exports = router;
